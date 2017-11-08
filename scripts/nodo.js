6e583de0101bc01a21a57f4ca0136a3c20d0305f
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-08 16:44:36 -0300

    Feat: Criando o método Nodo

--- scripts/nodo.js
+++ scripts/nodo.js
@@ -4,3 +4,30 @@ var right = 2;
 var down = 3;
 var left = 4;
 
+function Nodo(px, py, dir) {
+	
+	var x, y, direc;
+	this.x = px;
+	this.y = py;
+	this.direc = dir;
+	
+	this.Move = function() {
+		switch (this.direc) {
+			case up:
+				this.y -= 1;
+				break;
+				
+			case down:
+				this.y += 1;
+				break;
+			
+			case right:
+				this.x += 1;
+				break;
+				
+			case left: 
+				this.x -= 1;
+				break;
+		}
+	};
+}
