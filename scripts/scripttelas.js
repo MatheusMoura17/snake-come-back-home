fe99eaac765f6cc030a214012b4424d88d0740c2
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-21 02:56:51 -0300

    style: corrigindo os codigos dos arquivos javascript scriptstelas e html main-menu

--- scripts/scripttelas.js
+++ scripts/scripttelas.js
@@ -6,11 +6,22 @@ var larguramenu=640;
 var canvas=document.getElementById("screen");
 var ctx=canvas.getContext("2d");
 
+var background=document.getElementById("background");
 var title=document.getElementById("title");
 var iniciar=document.getElementById("iniciar");
 var sair=document.getElementById("sair");
 
-MenuPrincipal();
+window.onresize = function () {
+    MenuPrincipal();
+}
+
+function BackgroundMenu() {
+    var img;
+    var x = parseInt((largura / 2) - (larguramenu / 2));
+    var y = parseInt((altura / 2) - (alturamenu / 2));
+	img=background;
+    ctx.drawImage(img, x, y, larguramenu, alturamenu);
+}
 
 function DesenharMenuPrincipal() {
     var y=parseInt((altura / 2) - (alturamenu / 2));
@@ -20,16 +31,17 @@ function DesenharMenuPrincipal() {
 	var	img3;
 
     img1=title;
-    ctx.drawImage(img1, x, y, larguramenu, alturamenu);
+    ctx.drawImage(img1, x, y);
 
     img2=iniciar;
-    ctx.drawImage(img2, x, y + 100, larguramenu, alturamenu);
+    ctx.drawImage(img2, x, y + 100);
 	
 	img3=sair;
-	ctx.drawImage(img3, x, y + 200, larguramenu, alturamenu)
+	ctx.drawImage(img3, x, y + 200)
 
 }
 
 function MenuPrincipal() {
+	BackgroundMenu()
 	DesenharMenuPrincipal();
 }
