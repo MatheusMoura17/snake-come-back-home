fda073a75d6d02eaf132f3f31f7174982871089b
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-21 00:31:58 -0300

    feature: criando uma funcao Background no arquivo javascript scripttelas

--- scripts/scripttelas.js
+++ scripts/scripttelas.js
@@ -5,3 +5,13 @@ var larguramenu = 300;
 
 var canvas = document.GetElementById("screen");
 var ctx = canvas.getContext("2d");
+
+function Background() {
+	altura = window.innerHeight;
+	largura = window.innerWidth;
+	canvas.setAttribute("height", altura);
+	canvas.setAttribute("width", largura);
+	var img = new Image();
+	img.src = "../imagens/game/text1.png";
+	cxt.drawImage(img, 0, 0);
+}
