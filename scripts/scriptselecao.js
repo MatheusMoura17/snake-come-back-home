e489c069c432b949dc789f3d6add58880a32d933
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-21 06:08:11 -0300

    style: corrigindo o codigo no arquivo selecao-de-modo.html; Feature: adcionando as imagens no arquivo javascript scriptselecao.js

--- scripts/scriptselecao.js
+++ scripts/scriptselecao.js
@@ -36,15 +36,43 @@ function atualizarPlanoDeFundo() {
 function desenharFaseDeSelecao() {
     var y=parseInt((altura / 2) - (canvas.height / 2));
 	var x=parseInt((largura / 2) - (canvas.width / 2));
-    var img1, img2, img3, img4, img5, img6, img7, img8;
+    var img1;
+	var	img2;
+	var img3;
+	var	img4;	
+	var	img5;
+	var	img6;
+	var	img7;	
+	var	img8;
 
     img1=title1;
-    ctx.drawImage(img1, x+, y, 1370, 300);
+    ctx.drawImage(img1, x + 800, y + 100, 500, 150);
+	
+	img2=f1;
+    ctx.drawImage(img2, x + 900, y + 300, 100, 100);
+	
+	img3=f2;
+    ctx.drawImage(img3, x + 1100, y + 300, 100, 100);
+	
+	img4=f3;
+    ctx.drawImage(img4, x + 1000, y + 450, 100, 100);
+	
+	img5=barra;
+    ctx.drawImage(img5, x + 650, y + 50, 10, 550);
+	
+	img6=title2;
+    ctx.drawImage(img6, x + 100, y + 100, 500, 150);
+	
+	img7=livre;
+    ctx.drawImage(img7, x + 280, y + 300, 100, 100);
+	
+	img8=voltar;
+    ctx.drawImage(img8, x + 50, y + 500, 150, 100);
 
 
 }
 
-function menuPrincipal() {
+function faseDeSelecao() {
 	atualizarPlanoDeFundo();
 	//backgroundMenu();
 	desenharFaseDeSelecao();
