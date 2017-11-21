bafc6bc7d80665843821b62539e187512a244400
Alexsandro Rodrigues <thealeehmy@gmail.com>
2017-11-21 01:17:29 -0300

    style: corrigindo variaveis e o código do arquivo javascript scripttelas

--- scripts/scripttelas.js
+++ scripts/scripttelas.js
@@ -1,32 +1,40 @@
 var altura;
 var largura;
-var alturamenu = 480;
-var larguramenu = 640;
+var alturamenu=480;
+var larguramenu=640;
 
-var canvas = document.GetElementById("screen");
-var ctx = canvas.getContext("2d");
+var canvas=document.GetElementById("screen");
+var ctx=canvas.getContext("2d");
+
+var title=document.GetElementById("title");
+var iniciar=document.GetElementById("iniciar");
+var sair=document.GetElementById("sair");
 
 function Background() {
-	altura = window.innerHeight;
-	largura = window.innerWidth;
+	altura=window.innerHeight;
+	largura=window.innerWidth;
 	canvas.setAttribute("height", altura);
 	canvas.setAttribute("width", largura);
-	var img = new Image();
-	img.src = "../imagens/game/text1.png";
-	cxt.drawImage(img, 0, 0);
+	
 }
 
 function DesenharMenuPrincipal() {
-    var y = parseInt((altura / 2) - (alturaMenu / 2));
-	var x = parseInt((largura / 2) - (larguraMenu / 2));
+    var y=parseInt((altura / 2) - (alturaMenu / 2));
+	var x=parseInt((largura / 2) - (larguraMenu / 2));
     var img;
 
-    img = new Image();
-    img.src = "../imagens/game/play.png";
+    img=title;
     ctx.drawImage(img, x, y);
 
-    img = new Image();
-    img.src = "../imagens/game/quit.png";
+    img=iniciar);
     ctx.drawImage(img, x, y + 100);
+	
+	img=sair;
+	ctx.drawImage(img, x, y + 200)
 
+}
+
+function MenuPrincipal() {
+	Background();
+	DesenharMenuPrincipal();
 }
